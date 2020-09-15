d4f36e8d3e987202e841e490bc428a0919a208af
MaximeCgn <maxime.cugny@laposte.net>
2020-09-15 15:49:54 +0200

    V3

--- 01 - PHP CLI/03 EXOS ALGO A REFAIRE/3.6.php
+++ 01 - PHP CLI/03 EXOS ALGO A REFAIRE/3.6.php
@@ -1,38 +1,36 @@
 <?php
 
-    // $age=readline ("Entrez votre âge");
-    
-    //     if ($age==6 OR $age==7) {
-    //         echo "Vous entrez dans la catégorie Poussin.";
-    //     }
-    //     else if ($age==8 OR $age==9) {
-    //         echo "Vous entrez dans la catégorie Pupille";
-    //     }
-    //     else if($age==10 OR $age==11) {
-    //         echo "Vous entrez dans la catégorie Minime.";
-    //     }
-    //     else  if ($age>12) {
-    //         echo "Vous entrez dans la catégorie Cadet.";
-    //     }
+// $age=readline ("Entrez votre âge");
 
-    $age=readline("Entrez votre âge :");
+//     if ($age==6 OR $age==7) {
+//         echo "Vous entrez dans la catégorie Poussin.";
+//     }
+//     else if ($age==8 OR $age==9) {
+//         echo "Vous entrez dans la catégorie Pupille";
+//     }
+//     else if($age==10 OR $age==11) {
+//         echo "Vous entrez dans la catégorie Minime.";
+//     }
+//     else  if ($age>12) {
+//         echo "Vous entrez dans la catégorie Cadet.";
+//     }
 
-    switch ($age) {
-        case $age<=5:
-            echo "Vous êtes trop jeune.";
-            break;
-        case $age==6 OR $age==7:
-            echo "Vous entrez dans la catégorie Poussin.";
-            break;
-        case $age==8 OR $age==9:
-            echo "Vous entrez dans la catégorie Pupille.";
-            break;
-        case $age==10 OR $age==11:
-            echo "Vous entrez dans la catégorie Minime.";
-            break;
-        case $age>=12:
-            echo "Vous entrez dans la catégorie Cadet.";
-            break;
-    }
+$age = readline("Entrez votre âge :");
 
-?>
+switch ($age) {
+    case $age <= 5:
+        echo "Vous êtes trop jeune.";
+        break;
+    case $age == 6 or $age == 7:
+        echo "Vous entrez dans la catégorie Poussin.";
+        break;
+    case $age == 8 or $age == 9:
+        echo "Vous entrez dans la catégorie Pupille.";
+        break;
+    case $age == 10 or $age == 11:
+        echo "Vous entrez dans la catégorie Minime.";
+        break;
+    case $age >= 12:
+        echo "Vous entrez dans la catégorie Cadet.";
+        break;
+}
