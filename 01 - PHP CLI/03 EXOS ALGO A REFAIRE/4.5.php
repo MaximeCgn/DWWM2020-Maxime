71cb56936ee39b1affecf34c38362fa8c6b126bc
MaximeCgn <maxime.cugny@laposte.net>
2020-09-15 16:03:21 +0200

    V5

--- 01 - PHP CLI/03 EXOS ALGO A REFAIRE/4.5.php
+++ 01 - PHP CLI/03 EXOS ALGO A REFAIRE/4.5.php
@@ -23,26 +23,23 @@
 // }
 
 $error = 0;
-
+/*Boucle pour répéter les instructions dans le cas ou l'utilisateur ne respecte pas les règles*/
 while ($error == 0) {
-    $sexe = readline("Vous êtes un homme ou une femme ?\n");
+    $sexe = readline("Vous êtes un homme ou une femme ?\n"); /*Requête pour définir $sexe et $age*/
     $age = readline("Quel âge avez-vous ?\n");
     if ($sexe == "femme" or $sexe == "homme") {
         $error = 1;
         if (ctype_alpha($age)) {
             echo "Merci d'entrer un âge valide.\n";
-            $error=0;
-        }
-         else if (($sexe == "homme" and $age >= 20) or ($sexe == "femme" and $age >= 18 and $age <= 35)) {
+            $error = 0;
+        } else if (($sexe == "homme" and $age >= 20) or ($sexe == "femme" and $age >= 18 and $age <= 35)) {
             echo "Vous êtes imposable\n";
         } else {
             echo "Vous n'êtes pas imposable.\n";
         }
-    }
-    else if ($sexe != "femme" or $sexe != "homme") {
+    } else if ($sexe != "femme" or $sexe != "homme") {
         echo "Veuillez entrer un sexe valide.\n";
         $error = 0;
     }
-    
+
 }
-?>
