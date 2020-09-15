7328b066e3bbfefc6a95879e62099db971b45dc0
MaximeCgn <maxime.cugny@laposte.net>
2020-09-15 09:35:45 +0200

    V2

--- 01 - PHP CLI/03 EXOS ALGO A REFAIRE/4.7.php
+++ 01 - PHP CLI/03 EXOS ALGO A REFAIRE/4.7.php
@@ -5,37 +5,37 @@
     $acc=readline("De combien d'accidents êtes vous responsable ?");
     $fid=readline("Depuis combien d'années êtes vous client dans notre compagnie ?");
 
-    if ($age<25 AND $temps<2 AND $acc=0) {
+    if ($age<25 AND $temps<2 AND $acc==0) {
         echo "Vous bénéficiez du tarif rouge.";
             if ($fid>=1) {
                 echo "Vous pouvez passer au tarif orange.";
             }
     }
-    else if (($age<25 AND $temps>=2 AND $acc=0) OR ($age>=25 AND $temps<2 AND $acc=0)){
+    else if (($age<25 AND $temps>=2 AND $acc==0) OR ($age>=25 AND $temps<2 AND $acc==0)){
         echo "Vous bénéficiez du tarif orange.";
             if ($fid>=1) {
                 echo "Vous pouvez passer au tarif vert.";
             } 
     }
-    else if (($age<25 AND $temps>=2 AND $acc=0)OR($age>=25 AND $temps<2 AND $acc=0)) {
+    else if (($age<25 AND $temps>=2 AND $acc==0)OR($age>=25 AND $temps<2 AND $acc==0)) {
         echo "Vous bénéficiez du tarif rouge.";
             if ($fid>=1) {
                 echo "Vous pouvez passer au tarif orange.";
             }
     }
-    else if ($age>=25 AND $temps>=2 AND $acc=0) {
+    else if ($age>=25 AND $temps>=2 AND $acc==0) {
         echo "Vous bénéficiez du tarif vert.";
             if ($fid>=1) {
                 echo "Vous pouvez passer au tarif bleu.";
             }
     }
-    else if ($age>=25 AND $temps>=2 AND $acc=1) {
+    else if ($age>=25 AND $temps>=2 AND $acc==1) {
         echo "Vous bénéficiez du tarif orange.";
             if ($fid>=1) {
                 echo "Vous pouvez passer au tarif vert.";
             }
     }
-    else if ($age>=25 AND $temps>=2 AND $acc=2) {
+    else if ($age>=25 AND $temps>=2 AND $acc==2) {
         echo "Vous bénéficiez du tarif rouge";
             if ($fid>=1) {
                 echo "Vous pouvez passer au tarif orange.";
