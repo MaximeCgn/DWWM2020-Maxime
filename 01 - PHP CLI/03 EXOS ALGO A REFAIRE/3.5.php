eea64496edc9c87bbb43fd6deeb73e8b61e65338
MaximeCgn <maxime.cugny@laposte.net>
2020-09-15 11:46:13 +0200

    V2

--- 01 - PHP CLI/03 EXOS ALGO A REFAIRE/3.5.php
+++ 01 - PHP CLI/03 EXOS ALGO A REFAIRE/3.5.php
@@ -1,16 +1,23 @@
 <?php
 
-    $nbr1= readline ("Entrez le premier nombre");
-    $nbr2= readline ("Entrez le deuxième");
+    $nbr1= readline ("Entrez le premier nombre :");
+    $nbr2= readline ("Entrez le deuxième :");
 
-    if (($nbr1>0 and $nbr2>0) OR ($nbr1<0 and $nb2<0)) {
+    if (ctype_alpha($nbr1))  {
+        echo "Merci d'entrer un nombre.";
+    }
+    else if (ctype_alpha($nbr2)) {
+        echo "Merci d'entrer un nombre.";
+    }
+    else if (($nbr1>0 and $nbr2>0) OR ($nbr1<0 and $nbr2<0)) {
         echo "Le produit de ces nombres est positif.";
     }
-    else if ($nbr1=0 OR $nbr2=0) {
-        echo "Le produit de ces nombre est neutre.";
+    else if ($nbr1==0 OR $nbr2==0) {
+        echo "Le produit de ces nombres est neutre.";
     }
-    else {
-        echo "Le produit de ces nombre est négatif.";
+    else if ($nbr1<0 OR $nbr2<0) {
+        echo "Le produit de ces nombres est négatif.";
     }
+    
 
 ?>
