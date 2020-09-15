3cfa35360fc4ba4f147a340c43d11d2a1e40de0d
MaximeCgn <maxime.cugny@laposte.net>
2020-09-15 15:07:07 +0200

    V2

--- 01 - PHP CLI/03 EXOS ALGO A REFAIRE/4.8.php
+++ 01 - PHP CLI/03 EXOS ALGO A REFAIRE/4.8.php
@@ -4,7 +4,16 @@
     $mois=readline("Entrez un mois (exemple:4)");
     $an=readline("Entrez une année (exemple:1998)");
 
-    if ($an%4!=0 AND $an%400!=0 AND $mois==2 AND $jr>28) {
+    if (ctype_alpha($jr))  {
+        echo "Merci d'entrer des nombres.";
+    }
+    else if (ctype_alpha($mois))  {
+        echo "Merci d'entrer des nombres.";
+    }
+    else if (ctype_alpha($an))  {
+        echo "Merci d'entrer des nombres.";
+    }
+    else if ($an%4!=0 AND $an%400!=0 AND $mois==2 AND $jr>28) {
         echo "Cette date n'est pas valide";
     }
     else if ($an%4==0 AND $an%400==0 AND $mois==2 AND $jr>29) {
