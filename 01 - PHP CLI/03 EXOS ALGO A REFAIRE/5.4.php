afc74b0597574d76fd3a7c36337a9cba3db266b1
MaximeCgn <maxime.cugny@laposte.net>
2020-09-16 15:32:16 +0200

    V2

--- 01 - PHP CLI/03 EXOS ALGO A REFAIRE/5.4.php
+++ 01 - PHP CLI/03 EXOS ALGO A REFAIRE/5.4.php
@@ -1,6 +1,8 @@
 <?php
 
+    do {
     $nbr=readline("Inscrivez un nombre :"); /*Requête auprès de l'utilisateur pour définir $nbr*/
+    } while (ctype_alpha($nbr));
 
     FOR ($x=1; $x<=10; $x++) { /*Boucle pour calculer les tables de multiplication de $nbr*/
         echo $nbr,"x",$x,"=",$nbr*$x,"\n";
