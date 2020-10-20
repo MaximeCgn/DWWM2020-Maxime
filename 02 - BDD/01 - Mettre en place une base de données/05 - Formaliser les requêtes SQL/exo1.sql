a0f1788f9c52a7f4fa4cb105ecf8dadc87d8320e
59011-95-04 <59011-95-04@exchange.ad.afpanet>
2020-10-20 16:54:31 +0200

    20 10 2020

--- 02 - BDD/01 - Mettre en place une base de données/05 - Formaliser les requêtes SQL/exo1.sql
+++ 02 - BDD/01 - Mettre en place une base de données/05 - Formaliser les requêtes SQL/exo1.sql
@@ -25,7 +25,7 @@ SELECT UPPER (nomClient)
 FROM clients;
 
 /* G */
-SELECT CONCAT(UPPER(LEFT(nomClient,1)), SUBSTRING(nomClient,2)) as "Nom du Client"
+SELECT CONCAT(UPPER(LEFT(nomClient,1)), LOWER(SUBSTRING(nomClient,2))) as "Nom du Client"
 FROM clients;
 
 /* H */
