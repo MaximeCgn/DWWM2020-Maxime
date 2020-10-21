cf03f4d25d76a1d0914b5fb799766d4fab36cc38
59011-95-04 <59011-95-04@exchange.ad.afpanet>
2020-10-21 11:32:23 +0200

    21 10 2020

---
+++ 02 - BDD/01 - Mettre en place une base de données/04 - Formaliser les requêtes SQL/exo2.sql
@@ -0,0 +1,74 @@
+/* Exercice 2 */
+/* La colonne "Commande total" a été ajouté. Elle a été calculée en multipliant la quantite commandée par le prix de l'article commandée*/
+
+/* A */
+SELECT MAX(cde_total) as "Prix total maximum", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+
+/* B */
+SELECT FLOOR(AVG(cde_total)) as "Motant moyen des commandes", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+
+/* C */
+SELECT MIN(commandes.cde_total) as "Prix total minimum", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+
+/* D */
+SELECT SUM(commandes.quantiteCommande) as "Nombre de commandes passées"
+FROM commandes
+
+/* E */
+SELECT FLOOR(AVG(cde_total)) as "Motant moyen des commandes", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+GROUP BY clients.idClient
+
+/* F */
+SELECT MAX(cde_total) as "Prix total maximum", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+GROUP BY clients.idClient
+
+/* G */
+SELECT COUNT(commandes.idCommande) as "Nombre de commandes passées", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+GROUP BY clients.idClient
+
+/* H */
+SELECT FLOOR(AVG(commandes.quantiteCommande)) as "Nombre d'articles commandés en moyenne", UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+GROUP BY clients.idClient
+
+/* I */
+SELECT FLOOR(AVG(commandes.quantiteCommande)) as "Nombre d'articles commandés en moyenne", articles.descriptionArticle as "Description de l'article"
+FROM commandes INNER JOIN articles ON commandes.idArticle=articles.idArticle
+GROUP BY articles.idArticle
+
+/* J */
+SELECT SUM(commandes.quantiteCommande) as "Nombre d'articles commandés en moyenne", articles.descriptionArticle as "Description de l'article"
+FROM commandes INNER JOIN articles ON commandes.idArticle=articles.idArticle
+GROUP BY articles.idArticle
+
+/* K */
+SELECT FLOOR(AVG(commandes.quantiteCommande)) as "Nombre moyen d'articles commandées",  UPPER(clients.nomClient) as "Nom du Client", CONCAT(UPPER(LEFT(clients.prenomClient,1)), LOWER(SUBSTRING(clients.prenomClient,2))) as "Prénom du Client", commandes.idClient, commandes.dateCommande as "Date de la commande"
+FROM commandes INNER JOIN clients ON commandes.idClient=clients.idClient
+GROUP BY clients.nomClient, clients.prenomClient
+
+/* L */
+SELECT COUNT(commandes.idCommande) as "Quantité commandée", commandes.dateCommande as "Date de la commande"
+FROM commandes
+GROUP BY commandes.dateCommande
+
+/* M */
+SELECT COUNT(clients.idClient) as "Nombre de clients"
+FROM clients
+
+/* N */
+SELECT COUNT(DISTINCT commandes.idClient) as "Nombre de clients différents"
+FROM commandes 
+
+/* O */
+SELECT COUNT(DISTINCT commandes.idArticle) as "Nombre d'articles différents commandés"
+FROM commandes
+
+/* P */
+SELECT COUNT(DISTINCT commandes.dateCommande) as "Nombre de jours de commandes différents"
+FROM commandes
