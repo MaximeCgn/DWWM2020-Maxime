ae968d74fd91b5f68522ee3a312cf3e388a0eef1
MaximeCgn <maxime.cugny@laposte.net>
2020-11-19 17:34:19 +0100

    19/11/2020 17:34:19,56

--- 04 - PHP WEB/08 - CRUD/PHP/VIEW/modifier.php
+++ 04 - PHP WEB/08 - CRUD/PHP/VIEW/modifier.php
@@ -1,10 +1,10 @@
 <?php
 
-include ('head.php');
-include ('header.php');
+include 'head.php';
+include 'header.php';
 
 $idRecherche = $_GET['id'];
-$id=ProduitsManager::findById($idRecherche);
+$id = ProduitsManager::findById($idRecherche);
 
 echo '<div></div>';
 echo '<div class="titre liste"><h1>Modifier un produit</h1></div>';
@@ -12,16 +12,15 @@ echo '<div></div>';
 echo '<div class="colonne">';
 echo '<form method="POST" action="modifierTraitement.php">';
 echo '<div class="titre liste"><h2>Libellé du produit</h2></div>';
-echo '<input type="hidden" name="id" value="'.$id->getIdProduit().'"/>';
-echo '<input type="text" class="libelle listeDetail" name="libelleProduit" value="'.$id->getLibelleProduit().'"/>';
+echo '<input type="hidden" name="id" value="' . $id->getIdProduit() . '"/>';
+echo '<input type="text" class="libelle listeDetail" name="libelleProduit" value="' . $id->getLibelleProduit() . '"/>';
 echo '<div class="titre liste"><h2>Prix du produit</h2></div>';
-echo '<input type="text" class="libelle listeDetail" name="prix" value="'.$id->getPrix().'"/>';
+echo '<input type="text" class="libelle listeDetail" name="prix" value="' . $id->getPrix() . '"/>';
 echo '<div class="titre liste"><h2>Date de péremption du produit</h2></div>';
-echo '<input type="text" class="libelle listeDetail" name="dateDePeremption" value="'.$id->getDateDePeremption().'"/>';
+echo '<input type="text" class="libelle listeDetail" name="dateDePeremption" value="' . $id->getDateDePeremption() . '"/>';
 echo '<a href="../../index.php"><div class="return">RETOUR</div></a>';
 echo '<input type="submit" class="ajouter PadMarg" name="submit" value="MODIFIER"/>';
 echo '</form>';
-echo'</div>';
+echo '</div>';
 
-
-include ('footer.php');
+include 'footer.php';
