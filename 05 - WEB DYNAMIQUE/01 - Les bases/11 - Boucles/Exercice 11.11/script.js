ff19a39b86f9efe0a6861fa8e0a4c61eecb4f19d
59011-95-04 <59011-95-04@exchange.ad.afpanet>
2020-12-09 17:25:06 +0100

    09/12/2020 17:25:06,22

--- 05 - WEB DYNAMIQUE/01 - Les bases/11 - Boucles/Exercice 11.11/script.js
+++ 05 - WEB DYNAMIQUE/01 - Les bases/11 - Boucles/Exercice 11.11/script.js
@@ -7,14 +7,20 @@ do {
     switch (reponse) {
       case reponse > magic:
         alert("Trop grand !");
-        
+        prompt("Trouvez le nombre magique, entrez un nombre :")
         break;
       case reponse < magic:
         alert("Trop petit !");
+        prompt("Trouvez le nombre magique, entrez un nombre :")
+        break;
       case reponse == magic:
         alert("Vous avez gagné !");
+        prompt("Trouvez le nombre magique, entrez un nombre :")
+        break;
     }
   } while (reponse != magic);
 
   var rejouer = confirm("Prêt à rejouer ?");
 } while (rejouer == true);
+
+//PAFINI
