e2ad4fdb85efcb868b3857f2aaa872dba24aa584
59011-95-04 <59011-95-04@exchange.ad.afpanet>
2020-12-17 17:20:04 +0100

    17/12/2020 17:20:03,34

---
+++ backup/20201217/backup_eval_20201217.sql
@@ -0,0 +1,145 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eval
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `eval`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `eval` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `eval`;
+
+--
+-- Table structure for table `eleves`
+--
+
+DROP TABLE IF EXISTS `eleves`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `eleves` (
+  `idEleve` int(11) NOT NULL AUTO_INCREMENT,
+  `nomEleve` varchar(50) NOT NULL,
+  `prenomEleve` varchar(50) NOT NULL,
+  `classe` varchar(50) NOT NULL,
+  PRIMARY KEY (`idEleve`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eleves`
+--
+
+LOCK TABLES `eleves` WRITE;
+/*!40000 ALTER TABLE `eleves` DISABLE KEYS */;
+INSERT INTO `eleves` VALUES (1,'Totoo','Dupont','Terminale'),(2,'Tata','Durant','Seconde'),(3,'Tutu','Dupond','Première'),(5,'tata','tata','Terminale');
+/*!40000 ALTER TABLE `eleves` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `matieres`
+--
+
+DROP TABLE IF EXISTS `matieres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matieres` (
+  `idMatiere` int(11) NOT NULL AUTO_INCREMENT,
+  `libelleMatiere` varchar(50) NOT NULL,
+  PRIMARY KEY (`idMatiere`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matieres`
+--
+
+LOCK TABLES `matieres` WRITE;
+/*!40000 ALTER TABLE `matieres` DISABLE KEYS */;
+INSERT INTO `matieres` VALUES (1,'Français'),(2,'Anglais'),(3,'Maths'),(99,'Proviseur');
+/*!40000 ALTER TABLE `matieres` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suivis`
+--
+
+DROP TABLE IF EXISTS `suivis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suivis` (
+  `idSuivi` int(11) NOT NULL AUTO_INCREMENT,
+  `note` int(11) NOT NULL,
+  `coefficient` int(11) NOT NULL,
+  `idMatiere` int(11) NOT NULL,
+  `idEleve` int(11) NOT NULL,
+  PRIMARY KEY (`idSuivi`),
+  KEY `FK_suivis_matieres` (`idMatiere`),
+  KEY `FK_suivis_eleves` (`idEleve`),
+  CONSTRAINT `FK_suivis_eleves` FOREIGN KEY (`idEleve`) REFERENCES `eleves` (`idEleve`),
+  CONSTRAINT `FK_suivis_matieres` FOREIGN KEY (`idMatiere`) REFERENCES `matieres` (`idMatiere`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suivis`
+--
+
+LOCK TABLES `suivis` WRITE;
+/*!40000 ALTER TABLE `suivis` DISABLE KEYS */;
+INSERT INTO `suivis` VALUES (1,15,1,1,1),(2,15,1,2,2),(3,11,1,3,3),(5,15,2,1,5);
+/*!40000 ALTER TABLE `suivis` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateurs` (
+  `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(50) NOT NULL,
+  `nomUtilisateur` varchar(50) NOT NULL,
+  `prenomUtilisateur` varchar(50) NOT NULL,
+  `motDePasse` varchar(50) NOT NULL,
+  `role` int(11) NOT NULL,
+  `idMatiere` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idUtilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateurs`
+--
+
+LOCK TABLES `utilisateurs` WRITE;
+/*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
+INSERT INTO `utilisateurs` VALUES (1,'fr','fr','fr','fr',1,1),(2,'en','en','en','en',1,2),(3,'math','math','math','math',1,3),(4,'pr','pr','pr','pr',2,99);
+/*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-17 17:10:01
