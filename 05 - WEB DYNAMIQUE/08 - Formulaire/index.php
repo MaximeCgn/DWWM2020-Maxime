6a085164178d49482fc24d311d966433b59eff47
MaximeCgn <maxime.cugny@laposte.net>
2021-01-06 12:20:01 +0100

    06/01/2021 12:20:01,36

--- 05 - WEB DYNAMIQUE/08 - Formulaire/index.php
+++ 05 - WEB DYNAMIQUE/08 - Formulaire/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Titre de la page</title>
     <link rel="stylesheet" href="style.css">
-    
+
 
 </head>
 
@@ -21,85 +21,171 @@
             <form method="POST" action="action.php">
                 <div class="marge">
                     <div>
+                        <h3>Civilité :</h3>
+                    </div>
+                    <div>
+                        <div>
+                            <input type="radio" id="M" name="civilite" value="M." checked />
+                            <label for="M">M.</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="Mme" name="civilite" value="Mme" />
+                            <label for="Mme">Mme</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="Autre" name="civilite" value="Autre" />
+                            <label for="Autre">Autre</label>
+                        </div>
+                    </div>
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire</div>
+                    </div>
+                    <div class="taille">
+                        <img src="" class="correct">
+                    </div>
+                    <div class="pasCentrer" id="erreurNom"></div>
+                </div>
+
+                <div class="marge">
+                    <div>
                         <h3>Nom :</h3>
                     </div>
-                        <input type="text" id="nom" title="3 caractères minimum, lettres uniquements" pattern="[A-Za-z-']{3,}" required />
-                        <div class="question">
-                            <img class="infoBulle" src="question.png">
-                            <div class="cache" >Champ obligatoire, 3 caractères minimum, lettres uniquements.</div>
-                        </div>
-                        <div class="taille">
+                    <input type="text" name="nom" id="nom" title="3 caractères minimum, lettres uniquements"
+                        pattern="[A-Z]{3,}" required />
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire, 3 caractères minimum, lettres uniquements.</div>
+                    </div>
+                    <div class="taille">
                         <img src="" class="correct">
-                        </div>
-                        <div class="pasCentrer" id="erreurNom"></div>
+                    </div>
+                    <div class="pasCentrer" id="erreurNom"></div>
                 </div>
-                
+
+                <div class="marge">
+                    <div>
+                        <h3>Prénom :</h3>
+                    </div>
+                    <input type="text" name="prenom" id="prenpom" title="3 caractères minimum, lettres uniquements"
+                        pattern="[A-Za-z-']{3,}" required />
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire, 3 caractères minimum, lettres uniquements.</div>
+                    </div>
+                    <div class="taille">
+                        <img src="" class="correct">
+                    </div>
+                    <div class="pasCentrer" id="erreurPrenom"></div>
+                </div>
+
                 <div class="marge">
                     <div>
                         <h3>Date de naissance :</h3>
                     </div>
-                        <input type="date" id="ddn" title="Champ obligatoire" pattern="\d{4}$(\/)(((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])" required />
-                        <div class="question">
-                            <img class="infoBulle" src="question.png">
-                            <div class="cache" >Champ obligatoire.</div>
-                        </div>
-                        <div class="taille">
+                    <input type="date" id="ddn" name="ddn" title="Champ obligatoire"
+                        pattern="\d{4}$(\/)(((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])" required />
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire.</div>
+                    </div>
+                    <div class="taille">
                         <img src="" class="correct">
-                        </div>
-                        <div class="pasCentrer" id="erreurDDN"></div>
+                    </div>
+                    <div class="pasCentrer" id="erreurDDN"></div>
                 </div>
-                
+
                 <div class="marge">
                     <div>
                         <h3>Code Postal :</h3>
                     </div>
-                        <input type="text" title="5 chiffres attendus" pattern="[0-9]{5}" id="postal"  />
-                        <div class="question">
-                            <img class="infoBulle" src="question.png">
-                            <div class="cache">5 chiffres attendus.</div>
-                        </div>
-                        <div class="taille">
+                    <input type="text" title="5 chiffres attendus" name="codePostal" pattern="[0-9]{5}" id="postal" required />
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">5 chiffres attendus.</div>
+                    </div>
+                    <div class="taille">
                         <img src="" class="correct">
-                        </div>
-                        <div class="pasCentrer" id="erreurCP"></div>
+                    </div>
+                    <div class="pasCentrer" id="erreurCP"></div>
                 </div>
 
                 <div class="marge">
                     <div>
                         <h3>Adresse mail :</h3>
                     </div>
-                        <input type="text" title="Adresse mail attendue" id="email" pattern="([a-z0-9]+)@([\da-z]+)\.([a-z\.]{2,4})$"  required/>
-                        <div class="question">
-                            <img class="infoBulle" src="question.png">
-                            <div class="cache">Champ obligatoire, adresse mail correcte attendue</div>
-                        </div>
-                        <div class="taille">
+                    <input type="text" name="mail" title="Adresse mail attendue" id="email"
+                        pattern="([a-z0-9]+)@([\da-z]+)\.([a-z\.]{2,4})$" required />
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire, adresse mail correcte attendue</div>
+                    </div>
+                    <div class="taille">
                         <img src="" class="correct">
-                        </div>
-                        <div class="pasCentrer" id="erreurMail"></div>
+                    </div>
+                    <div class="pasCentrer" id="erreurMail"></div>
                 </div>
 
                 <div class="marge">
                     <div>
                         <h3>Mot de passe :</h3>
                     </div>
-                        <input type="password" title="8 caractères, 1 majuscule, 1 miniscule, 1 caractère spécial" id="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#\$%\^&\*+])[a-zA-Z\d!@#\$%\^&\*+]{8,}"  required/>
-                        
-                        <div class="question">
-                            <img class="infoBulle" src="question.png">
-                            <div class="cache">Champ obligatoire,8 caractères minimum dont 1 majuscule, 1 minuscule, 1 caractère spécial</div>
-                        </div>
-                        <div class="petiteTaille">
-                            <img src="oeil.png" id="oeil">
-                        </div>
-                        <div class="taille">
+                    <input type="password" name="mdp" title="8 caractères, 1 majuscule, 1 miniscule, 1 caractère spécial"
+                        id="password"
+                        pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#\$%\^&\*+])[a-zA-Z\d!@#\$%\^&\*+]{8,}"
+                        required />
+
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire,8 caractères minimum dont 1 majuscule, 1 minuscule, 1
+                            caractère spécial</div>
+                    </div>
+                    <div class="petiteTaille">
+                        <img src="oeil.png" id="oeil">
+                    </div>
+                    <div class="taille">
                         <img src="" class="correct">
-                        </div>
-                        <div class="pasCentrer" id="erreurPw"></div>
+                    </div>
+                    <div class="pasCentrer" id="erreurPw"></div>
+                </div>
+
+                <div class="marge">
+                    <div>
+                        <h3>Vérification du mot de passe :</h3>
+                    </div>
+                    <input type="password" name="verif" title="8 caractères, 1 majuscule, 1 miniscule, 1 caractère spécial"
+                        id="verifPassword"
+                        pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#\$%\^&\*+])[a-zA-Z\d!@#\$%\^&\*+]{8,}"
+                        required />
+
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Champ obligatoire,il doit correspondre au mot de passe du dessus</div>
+                    </div>
+                    <div class="taille">
+                        <img src="" class="correct">
+                    </div>
+                    <div class="pasCentrer" id="erreurVerif"></div>
+                </div>
+
+                <div class="marge">
+                    <div>
+                        <h3>Numéro de téléphone :</h3>
+                    </div>
+                    <input type="text" name="tel" title="Numéro de téléphone attendu" id="phone"
+                        pattern="(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}" />
+                    <div class="question">
+                        <img class="infoBulle" src="question.png">
+                        <div class="cache">Numéro de téléphone valide attendu</div>
+                    </div>
+                    <div class="taille">
+                        <img src="" class="correct">
+                    </div>
+                    <div class="pasCentrer" id="erreurPhone"></div>
                 </div>
                 <div class="marge">
                     <div></div>
-                    <input type="submit" id="submit" />
+                    <input type="submit" id="submit" disabled />
                     <div></div>
                 </div>
             </form>
--- 05 - WEB DYNAMIQUE/08 - Formulaire/script.js
+++ 05 - WEB DYNAMIQUE/08 - Formulaire/script.js
@@ -1,83 +1,180 @@
-/****Variables ****/
+/*****************************VARIABLES*****************************/
+/**Get input**/
 var nomP = document.getElementById("nom");
 var codePostal = document.getElementById("postal");
 var dateN = document.getElementById("ddn");
 var mail = document.getElementById("email");
 var mdp = document.getElementById("password");
+var tel= document.getElementById("phone");
+var verif=document.getElementById("verifPassword");
+var prenom=document.getElementById("prenom");
 
+/**Get erreur**/
 var erreurCode = document.getElementById("erreurCP");
 var erreurN = document.getElementById("erreurNom");
 var erreurM=document.getElementById("erreurMail");
 var erreurMdp=document.getElementById("erreurPw");
+var erreurTel=document.getElementById("erreurPhone");
+var erreurVer=document.getElementById("erreurVerif");
+var erreurP=document.getElementById("erreurPrenom");
 
+/**Submit**/
 var sub = document.getElementById("submit");
 
+/**Check image**/
 var checking = document.getElementsByClassName("correct");
 
+/**Infobulle**/
 var questions = document.getElementsByClassName("infoBulle");
+
+/**Oeil pour mdp**/
 var see=document.getElementById("oeil");
 
+/**Tableau de vérification**/
+var tabVerif={
+    "name":1,
+    "ddn":1,
+    "code":1,
+    "mail":1,
+    "mdp":1,
+    "verif":1,
+    "prenom":1
+}
 
-/***Fonctions ***/
+/*****************************FONCTIONS*****************************/
+/**Vérification dans le tableau, si tout le tableau est à 0, c'est bon**/
+function check() {
+    var taille=0;
+    var cpt=0;
+    for (var elt in tabVerif)
+    {
+        taille++;
+        if (tabVerif[elt]==0)
+        {
+            cpt++;
+        }
+    }
+    console.log(taille);
+    console.log(cpt);
+    if (cpt==taille)
+    {
+        sub.removeAttribute("disabled")
+    }
+    else {
+        sub.setAttribute("disabled","");
+    }
+}
 
-// function check() {
-//     if (flag1 == true && flag2 == true && flag3 == true && flag4 == true) {
-//         sub.removeAttribute("disabled");
-//     }
-// }
-/*****************************CHECKVALIDITY A FAIRE, OEIL, MESSAGE MDP COMPO**************************************/
+/**Véfification du nom**/
 function validateName() {
 
     if (nomP.checkValidity()) {
         erreurN.textContent = "";
-        checking[0].setAttribute("src", "check.png");
+        checking[1].setAttribute("src", "check.png");
+        tabVerif["name"]=0;
     }
     else {
-    checking[0].setAttribute("src", "cross.png");
+    checking[1].setAttribute("src", "cross.png");
     erreurN.textContent = "Le nom doit comporter au moins 3 caractères et des lettres uniquement.";
+    tabVerif["name"]=1;
     }
+    check();
 }
 
+/**Véfification du code postal**/
 function validateCP() {
     if (codePostal.checkValidity()) {
-        console.log(codePostal.checkValidity());
         erreurCP.textContent = "";
-        checking[2].setAttribute("src", "check.png");
+        checking[3].setAttribute("src", "check.png");
+        tabVerif["code"]=0;
     } else {
-    console.log(codePostal.checkValidity());
     erreurCode.textContent = "Le code postal doit comporter 5 caractères numériques."
-    checking[2].setAttribute("src", "cross.png");
+    checking[3].setAttribute("src", "cross.png");
+    tabVerif["code"]=1;
     }
+    check();
 }
 
+/**Véfification de la date de naissance**/
 function validateDate() {
     if (dateN.checkValidity()) {
-        checking[1].setAttribute("src", "check.png");
+        checking[2].setAttribute("src", "check.png");
+        tabVerif["ddn"]=0;
     } else {
-    checking[1].setAttribute("src", "cross.png");
+    checking[2].setAttribute("src", "cross.png");
+    tabVerif["ddn"]=1;
     }
+    check();
 }
 
+/**Véfification de l'adresse mail**/
 function validateMail() {
     if (mail.checkValidity()) {
         erreurM.textContent = "";
-        checking[3].setAttribute("src", "check.png");
+        checking[4].setAttribute("src", "check.png");
+        tabVerif["mail"]=0;
     } else {
     erreurM.textContent = "L'adresse mail doit suivre cet exemple : 'exemple@test.com'."
-    checking[3].setAttribute("src", "cross.png");
+    checking[4].setAttribute("src", "cross.png");
+    tabVerif["mail"]=1;
     }
+    check();
 }
 
+/**Véfification du mot de passe**/
 function validateMdp(){
     if (mdp.checkValidity()) {
         erreurMdp.textContent = "";
-        checking[4].setAttribute("src", "check.png");
+        checking[5].setAttribute("src", "check.png");
+        tabVerif["mdp"]=0;
     } else {
     erreurMdp.textContent = "8 caractères minimum dont 1 majuscule, 1 minuscule, 1 caractère spécial"
-    checking[4].setAttribute("src", "cross.png");
+    checking[5].setAttribute("src", "cross.png");
+    tabVerif["mdp"]=1;
+    }
+    check();
+}
+
+/**Véfification de la vérification du mot de passe**/
+function validateVerif(){
+    if(mdp.value==verif.value){
+        erreurVer.textContent="";
+        checking[6].setAttribute("src","check.png");
+        tabVerif["verif"]=0;
+    } else {
+        erreurVer.textContent="Champ obligatoire,il doit correspondre au mot de passe du dessus";
+        checking[6].setAttribute("src","cross.png");
+        tabVerif["verif"]=1;
+    }
+    check();
+}
+
+/**Véfification du numéro de téléphone**/
+function validatePhone(){
+    if(tel.value==""){
+        erreurTel.textContent="Numéro de téléphone valide attendu";
+        checking[7].setAttribute("src","cross.png");
+    } else if(tel.checkValidity){
+        erreurTel.textContent="";
+        checking[7].setAttribute("src","check.png");
+    } else {
+        erreurTel.textContent="Numéro de téléphone valide attendu";
+        checking[7].setAttribute("src","cross.png");
     }
 }
 
+/**Verification du prénom**/
+function validatePrenom(){
+    if(prenom.checkValidity()){
+        erreurTel.textContent="";
+        checking[7].setAttribute("src","check.png");
+    } else{
+        erreurTel.textContent="3 caractères minimum, lettres uniquements.";
+        checking[7].setAttribute("src","cross.png");
+    }
+}
+
+/**Affichage du mot de passe**/
 function voirMdp(){
     if (mdp.getAttribute("type")=="password")
     {
@@ -87,57 +184,13 @@ function voirMdp(){
     }
 }
 
-/***Event***/
-/**Check nom**/
-// nomP.addEventListener("change", function () {
-//     if (nomP.value.length < 3) {
-//         erreurN.textContent = "Le nom doit comporter au moins 3 caractères et des lettres uniquement.";
-//         checking[0].setAttribute("src","cross.png");
-//         check();
-//     }
-//     else {
-//         for (i = 0; i < nomP.value.length; i++) {
-//             if (!isNaN(nomP.value[i])) {
-//                 erreurN.textContent = "";
-//                 flag1 = true;
-//                 check();
-//                 erreurN.textContent = "Le nom doit comporter au moins 3 caractères et des lettres uniquement.";
-//                 flag1 = false;
-//                 checking[0].setAttribute("src","cross.png");
-//             }
-//             else {
-//                 erreurN.textContent = "";
-//                 flag1 = true;
-//                 check();
-//                 checking[0].setAttribute("src","check.png");
-//             }
-//         }
-//     }
-// });
+/**Empêche le copy paste sur la vérification du mot de passe**/
+function annule(event){
+    event.preventDefault()
+    return false;
+}
 
-// /**Check code postal**/
-// codePostal.addEventListener("change", function () {
-//     if (codePostal.value.length != 5) {
-//         erreurCode.textContent = "Le code postal doit comporter 5 caractères numériques.";
-//         flag2 = false;
-//         check();
-//         checking[2].setAttribute("src","cross.png");
-//     }
-//     else {
-//         erreurCode.textContent = "";
-//         flag2 = true;
-//         check();
-//         checking[2].setAttribute("src","check.png");
-//     }
-// });
-
-// /**Check de la date**/
-// dateN.addEventListener("change",function(){
-//     console.log("aaa");
-//     flag3=true;
-//     check();
-//     checking[1].setAttribute("src","check.png");
-// })
+/*****************************EVENEMENTS*****************************/
 
 /**Infos question mark**/
 for (i = 0; i < questions.length; i++) {
@@ -156,11 +209,14 @@ for (i = 0; i < questions.length; i++) {
 }
 
 /**REGEX TESTS**/
-
 nomP.addEventListener("blur", validateName);
 codePostal.addEventListener("blur", validateCP);
 dateN.addEventListener("blur", validateDate);
 mail.addEventListener("blur", validateMail);
 mdp.addEventListener("input", validateMdp);
+verif.addEventListener("blur",validateVerif)
+verif.addEventListener("paste",annule)
+tel.addEventListener("blur",validatePhone);
 
+/**Voir le mdp**/
 see.addEventListener("click",voirMdp);
