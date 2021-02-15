55784fbe6cdf63f4accc5d2e789f496ce67fc213
MaximeCgn <maxime.cugny@laposte.net>
2021-02-15 17:20:01 +0100

    15/02/2021 17:20:01,49

---
+++ backup/20210215/backup_riotapi_20210215.sql
@@ -0,0 +1,111 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: riotapi
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `riotapi`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `riotapi` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `riotapi`;
+
+--
+-- Table structure for table `historique`
+--
+
+DROP TABLE IF EXISTS `historique`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historique` (
+  `idHistorique` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idHistorique`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historique`
+--
+
+LOCK TABLES `historique` WRITE;
+/*!40000 ALTER TABLE `historique` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historique` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `points`
+--
+
+DROP TABLE IF EXISTS `points`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `points` (
+  `idPoints` int(11) NOT NULL AUTO_INCREMENT,
+  `points` int(11) NOT NULL,
+  PRIMARY KEY (`idPoints`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `points`
+--
+
+LOCK TABLES `points` WRITE;
+/*!40000 ALTER TABLE `points` DISABLE KEYS */;
+/*!40000 ALTER TABLE `points` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rencontres`
+--
+
+DROP TABLE IF EXISTS `rencontres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rencontres` (
+  `idRencontre` int(11) NOT NULL AUTO_INCREMENT,
+  `equipe1` varchar(100) NOT NULL,
+  `equipe2` varchar(100) NOT NULL,
+  `dateRencontre` datetime NOT NULL,
+  `idPoints` int(11) NOT NULL,
+  `idHistorique` int(11) NOT NULL,
+  PRIMARY KEY (`idRencontre`),
+  KEY `rencontres_points_FK` (`idPoints`),
+  KEY `rencontres_historique0_FK` (`idHistorique`),
+  CONSTRAINT `rencontres_historique0_FK` FOREIGN KEY (`idHistorique`) REFERENCES `historique` (`idHistorique`),
+  CONSTRAINT `rencontres_points_FK` FOREIGN KEY (`idPoints`) REFERENCES `points` (`idPoints`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rencontres`
+--
+
+LOCK TABLES `rencontres` WRITE;
+/*!40000 ALTER TABLE `rencontres` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rencontres` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-15 17:10:02
